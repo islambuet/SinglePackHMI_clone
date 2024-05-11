d7704228a4e8d0633d4269b4f9a33692c7153887
Md. Shaiful Islam <islambuet@gmail.com>
2024-05-11 21:49:21 +0600

    Motor Forward backward fixed

--- components/general/general_functions.js
+++ components/general/general_functions.js
@@ -75,6 +75,7 @@ function setMotorsLabel(){
     }
 }
 function setMotorDetailsView(){
+
     if(machine_mode!=1){
         $('#motor-details #tr_speed').hide();
         $('#motor-details #command_buttons_container').hide();
@@ -82,6 +83,18 @@ function setMotorDetailsView(){
     }
     else{
         $('#motor-details #tr_speed').show();
+        let motorForDetailsView=(basic_info['motors'][basic_info['selectedMachineId']+'_'+motorIdForDetailsView]);
+        $('#motor-details .button-motor-speed').hide();
+        if(motorForDetailsView!=undefined){
+            if(motorForDetailsView['bi_directional']==1){
+                $('#motor-details #button-motor-speed-forward').show();
+                $('#motor-details #button-motor-speed-backward').show();
+            }
+            else{
+                $('#motor-details #button-motor-speed-start').show();
+            }
+            $('#motor-details #button-motor-speed-stop').show();
+        }
         $('#motor-details #command_buttons_container').show();
         $('#motor-details').css('height','225px');
     }
@@ -164,55 +177,46 @@ $(document).on('click','.motor[clickable="1"]',function (event){
     motorIdForDetailsView=motor_id;
     setMotorDetailsView();
     $('#motor-details #speed_error').hide();
+
     $('#motor-details #motor_name').html(motors[basic_info['selectedMachineId']+'_'+motor_id]['motor_name'])
     $('#motor-details #location').html(motors[basic_info['selectedMachineId']+'_'+motor_id]['location'])
     $('#motor-details #ip_address').html(motors[basic_info['selectedMachineId']+'_'+motor_id]['ip_address'])
     $('#motor-details #current_speed').html('-')
     $('#motor-details').show();
 })
-$(document).on('click','#motor-details  #button-motor-start',function (event){
-    if(machine_mode==1){
+$(document).on('click','#motor-details  .button-motor-speed',function (event){
+    if(machine_mode==1) {
         let machine_id=basic_info['selectedMachineId'];
         let motorInfo=basic_info['motors'][machine_id+'_'+motorIdForDetailsView];
-        let speed;
+        let speed=0;
         let speed_min;
         let speed_max;
-        if(basic_info['hmiSettings']['motor_speed_unit']=='ft_min'){
-            speed=$('#motor-details #speed').val()*5.08;
-            speed_min=motorInfo['speed_min']/5.08;
-            speed_max=motorInfo['speed_max']/5.08;
+        let command=$(this).attr('data-command');
+        if(command>0){
+            if(basic_info['hmiSettings']['motor_speed_unit']=='ft_min'){
+                speed=$('#motor-details #speed').val()*5.08;
+                speed_min=motorInfo['speed_min']/5.08;
+                speed_max=motorInfo['speed_max']/5.08;
+            }
+            else{
+                speed=$('#motor-details #speed').val()*1000;
+                speed_min=motorInfo['speed_min']/1000;
+                speed_max=motorInfo['speed_max']/1000;
+            }
+            speed=parseInt(speed);
+            if(speed<motorInfo['speed_min'] || speed>motorInfo['speed_max']){
+                $('#motor-details #speed_max').text(speed_max.toFixed(2))
+                $('#motor-details #speed_min').text(speed_min.toFixed(2))
+                $('#motor-details #speed_error').show();
+                return;
+            }
         }
-        else{
-            speed=$('#motor-details #speed').val()*1000;
-            speed_min=motorInfo['speed_min']/1000;
-            speed_max=motorInfo['speed_max']/1000;
-        }
-        speed=parseInt(speed);
-
-        if(speed<motorInfo['speed_min'] || speed>motorInfo['speed_max']){
-            $('#motor-details #speed_max').text(speed_max.toFixed(2))
-            $('#motor-details #speed_min').text(speed_min.toFixed(2))
-            $('#motor-details #speed_error').show();
-        }
-        else{
-            $('#motor-details #speed_error').hide();
-            let params={
-                'message_id':123,
-                'device_id':(+motorIdForDetailsView + 100),
-                'command':1,
-                'parameter1':speed
-            };
-            ipcRenderer.send("sendRequestToServer", "forwardSMMessage",params,[]);
-        }
-    }
-})
-$(document).on('click','#motor-details  #button-motor-stop',function (event){
-    if(machine_mode==1) {
+        $('#motor-details #speed_error').hide();
         let params={
             'message_id':123,
-            'device_id':(+motorIdForDetailsView + 100),
-            'command':0,
-            'parameter1':0
+            'device_id':(+motorIdForDetailsView + 400),
+            'command':command,
+            'parameter1':speed
         };
         ipcRenderer.send("sendRequestToServer", "forwardSMMessage",params,[]);
     }
@@ -387,258 +391,5 @@ function setMotorsStates(data){
             $('#motor-details #cnx_status').css('fill',cnx_status);
             $('#motor-details #faulted_status').css('fill',faulted_status);
         }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-$('#switch_legend_production').change(function () {
-    if ($(this).is(":checked")) {
-        $('#svg_general_colors').hide();
-        $('#container_production').show();
-        ipcRenderer.send("sendRequestToIpcMain", "saveSettings",{'general_show_production':1});
-    } else {
-        $('#svg_general_colors').show();
-        $('#container_production').hide();
-        ipcRenderer.send("sendRequestToIpcMain", "saveSettings",{'general_show_production':0});
-    }
-});
-$('.bin.cursor-pointer').on('click',function (){
-    let bin_id=$(this).attr('bin-id');
-    if(basic_info['bins'][basic_info['selectedMachineId']+'_'+bin_id] !=undefined){
-        basic_info['pageParams']={'bin':basic_info['bins'][basic_info['selectedMachineId']+'_'+bin_id]}
-        ipcRenderer.send("sendRequestToIpcMain", "changeMenu",
-            {
-                'currentMenu':{'file':'general_bin_details',
-                'title':'Bins Detail: '+basic_info['pageParams']['bin']['bin_label'],
-                'name':'general_bin_details','members':'general'},
-                'pageParams':basic_info['pageParams']
-            });
-    }
-
-
-})
-
-function setBinsLabel(){
-    let bins=basic_info['bins']
-    let layoutNo=basic_info['hmiSettings']['general_layout_no']
-    if(bins!=undefined){
-        let num_bins=Math.max(...Object.values(bins).map(bin => bin['gui_id']!='999'?bin['gui_id']:0), 0);
-        let bin_width=0;
-        if(num_bins>0){
-            if(layoutNo=="5"){
-                bin_width=Math.trunc(1120/Math.ceil(num_bins/2))
-            }
-            else{
-                bin_width=Math.trunc(1500/Math.ceil(num_bins/2))
-            }
-        }
-        for(let key in bins){
-            if(bins[key]['gui_id']>0){
-                if(bins[key]['gui_id']!="999"){
-                    let binIndex=(Math.ceil(bins[key]['gui_id']/2));
-                    let posRect=0;
-                    let posText=0;
-                    if((layoutNo=="1")||(layoutNo=="3"))
-                    {
-                        posRect=201-1+(binIndex-1)*bin_width;
-                    }
-                    else if(layoutNo==5){
-                        posRect=1280-(binIndex)*bin_width;
-                    }
-                    else {
-                        posRect=1650-(binIndex)*bin_width;
-                    }
-                    posText=posRect-20+(bin_width/2);
-                    $('.bin[gui-bin-id='+bins[key]['gui_id']+'] rect').attr('width',bin_width-10).attr('x',posRect)
-                    $('.bin[gui-bin-id='+bins[key]['gui_id']+'] text').attr('x',posText);
-                }
-                $('.bin[gui-bin-id='+bins[key]['gui_id']+'] .bin-label').text(bins[key]['bin_label']);
-                $('.bin[gui-bin-id='+bins[key]['gui_id']+']').attr('bin-id',bins[key]['bin_id']).show();
-            }
-        }
-
-    }
-}
-
-
-
-function setTestButtonsStatus(outputStates){
-    let machine_id=basic_info['selectedMachineId'];
-    if(outputStates[machine_id+"_49"] && outputStates[machine_id+"_49"]['state']==1){
-        $("#btn-test-red-light").attr('data-started',1).css('background-color',$("#btn-test-red-light").attr('data-started-color'));
-    }
-    if(outputStates[machine_id+"_50"] && outputStates[machine_id+"_50"]['state']==1){
-        $("#btn-test-amber-light").attr('data-started',1).css('background-color',$("#btn-test-amber-light").attr('data-started-color'));
-    }
-    if(outputStates[machine_id+"_51"] && outputStates[machine_id+"_51"]['state']==1){
-        $("#btn-test-blue-light").attr('data-started',1).css('background-color',$("#btn-test-blue-light").attr('data-started-color'));
-    }
-}
-
-function setBinsStates(bin_states){
-    let bin_state_colors=basic_info['bin_state_colors'];
-    for(let bin_key in bin_states){
-        let bin_color='#27e22b';
-        for(let i=0;i<bin_state_colors.length;i++)
-        {
-            let bin_state_color=bin_state_colors[i];
-            if(bin_states[bin_key][bin_state_color['name']]==1){
-                bin_color=bin_state_color['color_active'];
-                break;
-            }
-        }
-        $('.bin[bin-id='+bin_states[bin_key]['bin_id']+'] rect').css('fill',bin_color);
-    }
-}
-
-
-function setDoorsStates(input_states){
-    let machine_id=basic_info['selectedMachineId'];
-    $('.door').hide();//hide all buttons
-    let doors=basic_info['doors']
-    for(let door_no in doors){
-        let door=doors[door_no];
-        let door_closed='in-active';
-        let door_locked='in-active';
-        let door_safe='in-active';
-        if(door[1] !=undefined){
-            if(input_states[machine_id+'_'+door[1]['input_id']] !=undefined){
-                if(input_states[machine_id+'_'+door[1]['input_id']]['state']==door[1]['active_state']){
-                    door_closed='active';
-                }
-            }
-        }
-        if(door[2]){
-            if(input_states[machine_id+'_'+door[2]['input_id']]){
-                if(input_states[machine_id+'_'+door[2]['input_id']]['state']==door[2]['active_state']){
-                    door_locked='active';
-                }
-            }
-        }
-        if(door[3]){
-            if(input_states[machine_id+'_'+door[3]['input_id']]){
-                if(input_states[machine_id+'_'+door[3]['input_id']]['state']==door[3]['active_state']){
-                    door_safe='active';
-                }
-            }
-        }
-        if(door_closed=='active'){
-            if((door_locked=='in-active')&&(door_safe=='in-active')){
-                $('.door-lock[data-device-id='+(+door_no+90)+']').show();
-            }
-            else{
-                $('.door-unlock[data-device-id='+(+door_no+90)+']').show();
-            }
-        }
-        else{
-            $('.door-open[data-device-id='+(+door_no+90)+']').show();
-        }
-
-    }
-}
-function setEstopsStates(input_states){
-    let input_colors = {"in-active" : "#00ff00", "active" : "#ff0000"};
-    for(let key in basic_info['inputs']){
-        let input=basic_info['inputs'][key];
-        if((input['input_type']==3) && input['gui_id']>0 &&  (input['device_type']==0) && (input['device_number']==0) ){
-            let state='in-active'
-            if(input_states[key] !=undefined){
-                if(input['active_state']==input_states[key]['state']){
-                    state='active'
-                }
-            }
-            $('.estop[input-id='+input["input_id"]+'] .status').css('fill',input_colors[state]);
-
-        }
-    }
-}
-
-
-function setStatisticsCounter(statistics_counter){
-    if(statistics_counter.length>0){
-        let shiftInfo=statistics_counter[0];
-        let machine_errors = 0, non_machine_errors = 0
-        Object.keys(shiftInfo).forEach(s_key => {
-            if (['sc1', 'sc3', 'sc4', 'sc6', 'sc9', 'sc14', 'sc16', 'sc17', 'sc21'].includes(s_key)) {
-                machine_errors += Number(shiftInfo[s_key]);
-            }
-            if (['sc5', 'sc7', 'sc8', 'sc10', 'sc12', 'sc18'].includes(s_key)) {
-                non_machine_errors += Number(shiftInfo[s_key]);
-            }
-        });
-
-        let production_data = {
-            total_read: {label: 'Total inducted', count: 0},
-            sc0: {label: 'Total good diverts', count: 0},
-            machine_error: {label: 'Total machine error packages', count: 0},
-            non_machine_error: {label: 'Total non-machine error packages', count: 0},
-        };
-        production_data['total_read']['count'] = shiftInfo['total_read'];
-        production_data['sc0']['count'] = shiftInfo['sc0'];
-        production_data['machine_error']['count'] = machine_errors;
-        production_data['non_machine_error']['count'] = non_machine_errors;
-        $('#table_production tbody').empty();
-        Object.keys(production_data).forEach(key => {
-            let html = '<tr>' + '<td>' + production_data[key]['label'] + '</td>' +
-                '<td class="text-right">' + production_data[key]['count'] + '</td>' +
-                '<td class="text-right">' + (shiftInfo['total_read'] > 0 ? (production_data[key]['count'] * 100 / shiftInfo['total_read']).toFixed(2) : '0') + '%' + '</td>'
-                + '</tr>';
-            $('#table_production tbody').append(html)
-        });
-
-        let scanner_data = {
-            valid: {label: 'Good scan', count: 0},
-            no_read: {label: 'No read', count: 0},
-            no_code: {label: 'No code', count: 0},
-            multiple_read: {label: 'Multiple read', count: 0},
-        };
-        scanner_data['valid']['count'] = shiftInfo['valid']
-        scanner_data['no_read']['count'] = shiftInfo['no_read']
-        scanner_data['no_code']['count'] = shiftInfo['no_code']
-        scanner_data['multiple_read']['count'] = shiftInfo['multiple_read']
-
-        $('#table_scanner tbody').empty();
-        Object.keys(scanner_data).forEach(key => {
-            let html = '<tr>' + '<td>' + scanner_data[key]['label'] + '</td>' +
-                '<td class="text-right">' + scanner_data[key]['count'] + '</td>' +
-                '<td class="text-right">' + (shiftInfo['total_read'] > 0 ? (scanner_data[key]['count'] * 100 / shiftInfo['total_read']).toFixed(2) : 0) + '%' + '</td>'
-                + '</tr>';
-            $('#table_scanner tbody').append(html)
-        });
-    }
-
-}
-function setStatisticsOee(statistics_oee){
-    if(statistics_oee.length>0){
-        let oeeInfo=statistics_oee[0];
-        let oee_data = {
-            cal_availability: {label: 'Availability'},
-            cal_quality: {label: 'Quality'},
-            cal_performance: {label: 'Performance'},
-            cal_oee: {label: 'OEE'},
-        };
-
-        oeeInfo['cal_availability']= (+oeeInfo['tot_sec_blocked']) + (+oeeInfo['tot_sec_estop']) + (+oeeInfo['tot_sec_fault']) + (+oeeInfo['tot_sec_run'])
-        oeeInfo['cal_availability']= (+oeeInfo['cal_availability'])>0? ((+oeeInfo['tot_sec_run'])/oeeInfo['cal_availability']).toFixed(2):'0';
-        oeeInfo['cal_quality']= (+oeeInfo['packages_inducted'])>0?((+oeeInfo['successful_divert_packages'])/(+oeeInfo['packages_inducted'])).toFixed(2):'0';
-        oeeInfo['cal_performance']= (+oeeInfo['tot_sec_run'])>0?((+oeeInfo['packages_inducted']) * (+oeeInfo['max_3min_tput'])/(+oeeInfo['tot_sec_run'])).toFixed(2):'0';
-        oeeInfo['cal_oee']= ((+oeeInfo['cal_availability']) * (+oeeInfo['cal_quality']) * (+oeeInfo['cal_performance'])).toFixed(2)
-
-        $('#table_oee tbody').empty();
-        Object.keys(oee_data).forEach(key => {
-            let html = '<tr>' + '<td>' + oee_data[key]['label'] + '</td>' +
-                '<td class="text-right">' + oeeInfo[key] + '</td>' +
-                + '</tr>';
-            $('#table_oee tbody').append(html)
-        });
     }
 }
